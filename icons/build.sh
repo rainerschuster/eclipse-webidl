9c771b515514add1ce82b7f239b387208cc18324
Rainer Schuster <rainer.schuster@gmail.com>
2015-01-07 17:23:04 +0100

    Updated buildscript to support transparent background and distribute icons. Regenerated icons.

--- icons/build.sh
+++ icons/build.sh
@@ -1,43 +1,54 @@
 
 # Branding - Window Images
-convert logo.svg -resize 16x16 eclipse16.gif
-convert logo.svg -resize 32x32 eclipse32.gif
-convert logo.svg -resize 48x48 eclipse48.gif
-convert logo.svg -resize 16x16 eclipse16.png
-convert logo.svg -resize 32x32 eclipse32.png
-convert logo.svg -resize 48x48 eclipse48.png
-convert logo.svg -resize 64x64 eclipse64.png
-convert logo.svg -resize 128x128 eclipse128.png
-convert logo.svg -resize 256x256 eclipse256.png
+convert -background none -resize 16x16 logo.svg eclipse16.gif
+convert -background none -resize 32x32 logo.svg eclipse32.gif
+convert -background none -resize 48x48 logo.svg eclipse48.gif
+convert -background none -resize 16x16 logo.svg eclipse16.png
+convert -background none -resize 32x32 logo.svg eclipse32.png
+convert -background none -resize 48x48 logo.svg eclipse48.png
+convert -background none -resize 64x64 logo.svg eclipse64.png
+convert -background none -resize 128x128 logo.svg eclipse128.png
+convert -background none -resize 256x256 logo.svg eclipse256.png
 
 
 # Launching - Program Launcher
 # Linux
-convert logo.svg -resize 256x256 icon.xpm
+convert -background none -resize 256x256 logo.svg icon.xpm
 # Mac OS X
-convert logo.svg -resize 16x16 icon_16x16.png
-convert logo.svg -resize 32x32 icon_16x16@2x.png
-convert logo.svg -resize 32x32 icon_32x32.png
-convert logo.svg -resize 128x128 icon_32x32@2x.png
-convert logo.svg -resize 128x128 icon_128x128.png
-convert logo.svg -resize 256x256 icon_128x128@2x.png
-convert logo.svg -resize 256x256 icon_256x256.png
-convert logo.svg -resize 512x512 icon_256x256@2x.png
-convert logo.svg -resize 512x512 icon_512x512.png
-convert logo.svg -resize 1024x1024 icon_512x512@2x.png
-convert logo.svg Eclipse.icns
+convert -background none -resize 16x16 logo.svg icon_16x16.png
+convert -background none -resize 32x32 logo.svg icon_16x16@2x.png
+convert -background none -resize 32x32 logo.svg icon_32x32.png
+convert -background none -resize 128x128 logo.svg icon_32x32@2x.png
+convert -background none -resize 128x128 logo.svg icon_128x128.png
+convert -background none -resize 256x256 logo.svg icon_128x128@2x.png
+convert -background none -resize 256x256 logo.svg icon_256x256.png
+convert -background none -resize 512x512 logo.svg icon_256x256@2x.png
+convert -background none -resize 512x512 logo.svg icon_512x512.png
+convert -background none -resize 1024x1024 logo.svg icon_512x512@2x.png
+convert -background none logo.svg Eclipse.icns
 # Solaris
-convert logo.svg Eclipse.l.pm
-convert logo.svg Eclipse.m.pm
-convert logo.svg Eclipse.s.pm
-convert logo.svg Eclipse.t.pm
+convert -background none logo.svg Eclipse.l.pm
+convert -background none logo.svg Eclipse.m.pm
+convert -background none logo.svg Eclipse.s.pm
+convert -background none logo.svg Eclipse.t.pm
 # Windows
-convert logo.svg -resize 16x16 -type Palette -colors 255 icon_16x16_8bit.bmp
-convert logo.svg -resize 32x32 -type Palette -colors 255 icon_32x32_8bit.bmp
-convert logo.svg -resize 48x48 -type Palette -colors 255 icon_48x48_8bit.bmp
-convert logo.svg -resize 16x16 icon_16x16_32bit.bmp
-convert logo.svg -resize 32x32 icon_32x32_32bit.bmp
-convert logo.svg -resize 48x48 icon_48x48_32bit.bmp
-convert logo.svg -resize 256x256 icon_256x256_32bit.bmp
+convert -background none -resize 16x16 -type Palette -colors 255 -channel Alpha logo.svg icon_16x16_8bit.bmp
+convert -background none -resize 32x32 -type Palette -colors 255 -channel Alpha logo.svg icon_32x32_8bit.bmp
+convert -background none -resize 48x48 -type Palette -colors 255 -channel Alpha logo.svg icon_48x48_8bit.bmp
+convert -background none -resize 16x16 logo.svg icon_16x16_32bit.bmp
+convert -background none -resize 32x32 logo.svg icon_32x32_32bit.bmp
+convert -background none -resize 48x48 logo.svg icon_48x48_32bit.bmp
+convert -background none -resize 256x256 logo.svg icon_256x256_32bit.bmp
 convert icon_16x16_8bit.bmp icon_16x16_32bit.bmp icon_32x32_8bit.bmp icon_32x32_32bit.bmp icon_48x48_8bit.bmp icon_48x48_32bit.bmp icon_256x256_32bit.bmp eclipse.ico
+
+
+
+cp icon.xpm ../com.rainerschuster.webidl.product/icons/
+# cp Eclipse.icns ../com.rainerschuster.webidl.product/icons/
+cp Eclipse.*.pm ../com.rainerschuster.webidl.product/icons/
+# cp eclipse.ico ../com.rainerschuster.webidl.product/icons/
+cp icon_*.bmp ../com.rainerschuster.webidl.product/icons/
+cp eclipse*.gif ../com.rainerschuster.webidl.rcp/icons/
+cp eclipse*.png ../com.rainerschuster.webidl.rcp/icons/
+cp eclipse32.png ../com.rainerschuster.webidl.ui/icons/logo.png
 
